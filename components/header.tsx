243d81440690f2d469f8cbf669284db435f2cb6b
Arvin Jayson Tamayo Castro <email@example.com>
2022-08-13 22:32:27 +0800

    Sidebar
    Sidebar

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,23 @@ import utilStyles from '../styles/utils.module.css'
 import styles from './header.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import React, { useState } from 'react';
 
 const logoAlt = 'AO'
 const logoText = 'AO'
 
 export default function Header() {
+
+  const [isMenu, setIsMenu] = useState(false);
+  
+  const contentClassname = isMenu
+  ? `${styles["nav-open"]} ${styles.sidebar}`
+  : styles.sidebar;
+
+  const menuClassName = isMenu
+  ? `${styles["nav-open"]} ${styles.menu} ${styles.sidebarmenu}`
+  : `${styles.menu} ${styles.sidebarmenu}`;
+
   return (
     <div className={styles.bar}>
       <div className={styles.container}>
@@ -32,7 +44,30 @@ export default function Header() {
               About
             </a>
           </Link>
+          <button className={styles.menu} onClick={() => setIsMenu(!isMenu)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </nav>
+      </div>
+      <div className={contentClassname}>
+          <h2>Menu</h2>
+          <button className={menuClassName} onClick={() => setIsMenu(!isMenu)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <Link href="/">
+            <a className={styles.link}>
+              Home
+            </a>
+          </Link>
+          <Link href="/about">
+            <a className={styles.link}>
+              About
+            </a>
+          </Link>
       </div>
     </div>
   )
